ba189dc0964cce652d078a69c1d3e9c5762f908e
Alexandre <alexandre.chevalier.1@ulaval.ca>
2015-03-25 12:09:02 -0400

    dépot du travail

---
+++ lib/ex09.dart
@@ -0,0 +1,100 @@
+library ex09;
+
+import 'dart:html';
+import 'package:boarding/pieces.dart';
+import 'package:boarding/util.dart';
+import 'package:boarding/boarding.dart';
+
+part 'view/board.dart';
+
+onOff(end) {
+}
+
+abstract class Car extends MovablePiece {
+  static const num defaultWidth = 75;
+  static const num defaultHeight = 30;
+
+  Car(int id) : super(id) {
+    shape = PieceShape.VEHICLE;
+    width = defaultWidth;
+    height = defaultHeight;
+  }
+}
+
+class NonRedCar extends Car {
+
+  NonRedCar(int id) : super(id) {
+    dx = randomRangeNum(1, 3);
+    dy = randomRangeNum(1, 3);
+    borderColor = '#3ADF00';
+    linePath.width = 3;
+    color = 'blue';
+  }
+}
+
+class RedCar extends Car {
+  static const num smallWidth = 35;
+  static const num smallHeight = 14;
+  static const num bigWidth = 75;
+  static const num bigHeight = 30;
+  static const String smallColorCode = '#FF00FF';
+  static const String bigColorCode = '#E40000';
+
+
+  bool isSmall = false;
+
+  RedCar(int id) : super(id) {
+    smaller();
+  }
+
+  bool get isBig => !isSmall;
+
+  move([Direction direction]) {
+    if (x > space.width - width) {
+      x = space.width - width;
+    }
+    if (x < 0) {
+      x = 0;
+    }
+    if (y > space.height - height) {
+      y = space.height - height;
+    }
+    if (y < 0) {
+      y = 0;
+    }
+  }
+
+  bigger() {
+    if (isSmall) {
+      isSmall = false;
+      width = bigWidth;
+      height = bigHeight;
+      color = bigColorCode;
+
+    }
+  }
+
+  smaller() {
+    if (isBig) {
+      isSmall = true;
+      width = smallWidth;
+      height = smallHeight;
+      color = smallColorCode;
+
+    }
+  }
+}
+
+class Cars extends MovablePieces {
+  RedCar redCar;
+
+  Cars(int count) : super(count) {
+    redCar = new RedCar(0);
+  }
+
+  createMovablePieces(int count) {
+    for (var i = 0; i < count - 1; i++) {
+      add(new NonRedCar(i + 1));
+    }
+  }
+}
---
+++ web/game.dart
@@ -0,0 +1,7 @@
+import 'dart:html';
+import 'package:ex09/ex09.dart';
+
+
+main() {
+  new Board(querySelector('#canvas')).draw();
+}
